410f3cf3fa69e4fc26247e37fbcc35df0f6fab34
kpcruz <kelvinmembership@gmail.com>
2020-02-10 13:18:03 -0800

    Saved.js converted to async/await

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -8,70 +8,74 @@ import { Col, Row, Container } from "../components/Grid";
 import { List } from "../components/List";
 
 function Saved() {
-  const [books, setBooks] = useState([]);
+	const [books, setBooks] = useState([]);
 
-  const getSavedBooks = () => {
-    API.getSavedBooks()
-      .then(res => setBooks(res.data))
-      .catch(err => console.log(err));
-  };
+	const getSavedBooks = async () => {
+		try {
+			const response = await API.getSavedBooks();
+			setBooks(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const handleBookDelete = id => {
-    API.deleteBook(id).then(res => getSavedBooks());
-  };
+	const handleBookDelete = async id => {
+		const response = await API.deleteBook(id);
+		getSavedBooks(response);
+	};
 
-  useEffect(() => {
-    getSavedBooks();
-  }, []);
+	useEffect(() => {
+		getSavedBooks();
+	}, []);
 
-  return (
-    <Container>
-      <Row>
-        <Col size="md-12">
-          <Jumbotron>
-            <h1 className="text-center">
-              <strong>(React) Google Books Search</strong>
-            </h1>
-            <h2 className="text-center">
-              Search for and Save Books of Interest.
-            </h2>
-          </Jumbotron>
-        </Col>
-      </Row>
-      <Row>
-        <Col size="md-12">
-          <Card title="Saved Books" icon="download">
-            {books.length ? (
-              <List>
-                {books.map(book => (
-                  <Book
-                    key={book._id}
-                    title={book.title}
-                    subtitle={book.subtitle}
-                    link={book.link}
-                    authors={book.authors.join(", ")}
-                    description={book.description}
-                    image={book.image}
-                    Button={() => (
-                      <button
-                        onClick={() => handleBookDelete(book._id)}
-                        className="btn btn-danger ml-2"
-                      >
-                        Delete
-                      </button>
-                    )}
-                  />
-                ))}
-              </List>
-            ) : (
-              <h2 className="text-center">No Saved Books</h2>
-            )}
-          </Card>
-        </Col>
-      </Row>
-      <Footer />
-    </Container>
-  );
+	return (
+		<Container>
+			<Row>
+				<Col size="md-12">
+					<Jumbotron>
+						<h1 className="text-center">
+							<strong>(React) Google Books Search</strong>
+						</h1>
+						<h2 className="text-center">
+							Search for and Save Books of Interest.
+						</h2>
+					</Jumbotron>
+				</Col>
+			</Row>
+			<Row>
+				<Col size="md-12">
+					<Card title="Saved Books" icon="download">
+						{books.length ? (
+							<List>
+								{books.map(book => (
+									<Book
+										key={book._id}
+										title={book.title}
+										subtitle={book.subtitle}
+										link={book.link}
+										authors={book.authors.join(", ")}
+										description={book.description}
+										image={book.image}
+										Button={() => (
+											<button
+												onClick={() => handleBookDelete(book._id)}
+												className="btn btn-danger ml-2"
+											>
+												Delete
+											</button>
+										)}
+									/>
+								))}
+							</List>
+						) : (
+							<h2 className="text-center">No Saved Books</h2>
+						)}
+					</Card>
+				</Col>
+			</Row>
+			<Footer />
+		</Container>
+	);
 }
 
 export default Saved;
