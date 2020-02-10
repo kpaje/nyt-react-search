6a4cc883c834122e19db3457ff0ab3d7c7242daf
kpcruz <kelvinmembership@gmail.com>
2020-02-10 05:35:37 -0800

    converted Home.js to functional component

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+	app.use(express.static("client/build"));
 }
 
 // Add routes, both API and view
@@ -17,11 +17,11 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
-  useCreateIndex: true,
-  useNewUrlParser: true
+	useCreateIndex: true,
+	useNewUrlParser: true
 });
 
 // Start the API server
 app.listen(PORT, () =>
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+	console.log(`🌎  ==> API Server now listening on http://localhost:${PORT}!`)
 );
