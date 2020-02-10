3cce230db2d8c701054717c589b0384df9e2f4d0
kpcruz <kelvinmembership@gmail.com>
2020-02-10 06:42:54 -0800

    refactored googleController for readability

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -2,35 +2,44 @@ const axios = require("axios");
 const db = require("../models");
 
 // Defining methods for the googleController
-// findAll searches the Google Books API and returns only the entries we haven't already saved
-// It also makes sure that the books returned from the API all contain a title, author, link, description, and image
+// findAll searches the Google Books API
+// It makes sure that the books returned from the API all contain a title, author, link, description, and image
+// It returns only the entries we haven't already saved
+
+const googleApiLink = "https://www.googleapis.com/books/v1/volumes";
+
+const filterForItemizedResults = results => {
+	return results.data.items.filter(
+		result =>
+			result.volumeInfo.title &&
+			result.volumeInfo.infoLink &&
+			result.volumeInfo.authors &&
+			result.volumeInfo.description &&
+			result.volumeInfo.imageLinks &&
+			result.volumeInfo.imageLinks.thumbnail
+	);
+};
+
+const verifyDuplicateBooksInDatabase = (dbBooks, apiBooks) => {
+	return apiBooks.filter(apiBook =>
+		dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
+	);
+};
+
 module.exports = {
-  findAll: function(req, res) {
-    const { query: params } = req;
-    const googleApiLink = "https://www.googleapis.com/books/v1/volumes";
-    axios
-      .get(googleApiLink, {
-        params
-      })
-      .then(results =>
-        results.data.items.filter(
-          result =>
-            result.volumeInfo.title &&
-            result.volumeInfo.infoLink &&
-            result.volumeInfo.authors &&
-            result.volumeInfo.description &&
-            result.volumeInfo.imageLinks &&
-            result.volumeInfo.imageLinks.thumbnail
-        )
-      )
-      .then(apiBooks =>
-        db.Book.find().then(dbBooks =>
-          apiBooks.filter(apiBook =>
-            dbBooks.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
-          )
-        )
-      )
-      .then(books => res.json(books))
-      .catch(err => res.status(422).json(err));
-  }
+	findAll: function(req, res) {
+		const { query: params } = req;
+		axios
+			.get(googleApiLink, {
+				params
+			})
+			.then(results => filterForItemizedResults(results))
+			.then(apiBooks =>
+				db.Book.find().then(dbBooks =>
+					verifyDuplicateBooksInDatabase(dbBooks, apiBooks)
+				)
+			)
+			.then(books => res.json(books))
+			.catch(err => res.status(422).json(err));
+	}
 };
