f41efba357053c327f7e0f4e0eb4cca58be82358
kpcruz <kelvinmembership@gmail.com>
2020-02-11 01:32:53 -0800

    jest snapshots progress

--- client/src/__tests__/components.test.js
+++ client/src/__tests__/components.test.js
@@ -1,7 +1,27 @@
 import React from "react";
 import { create } from "react-test-renderer";
+import Book from "../components/Book/index";
+import Card from "../components/Card/index";
 import Footer from "../components/Footer/index";
+import Form from "../components/Form/index";
+import Grid from "../components/Grid/index";
 import Jumbotron from "../components/Jumbotron/index";
+import List from "../components/List/index";
+import Nav from "../components/Nav/index";
+
+describe("Render Book component correctly", () => {
+	test("Matches the snapshot", () => {
+		const book = create(<Book />);
+		expect(book.toJSON()).toMatchSnapshot();
+	});
+});
+
+describe("Render Card component correctly", () => {
+	test("Matches the snapshot", () => {
+		const card = create(<Card />);
+		expect(card.toJSON()).toMatchSnapshot();
+	});
+});
 
 describe("Render Footer component correctly", () => {
 	test("Matches the snapshot", () => {
@@ -10,9 +30,37 @@ describe("Render Footer component correctly", () => {
 	});
 });
 
+describe("Render Form component correctly", () => {
+	test("Matches the snapshot", () => {
+		const form = create(<Form />);
+		expect(form.toJSON()).toMatchSnapshot();
+	});
+});
+
+describe("Render Grid component correctly", () => {
+	test("Matches the snapshot", () => {
+		const grid = create(<Grid />);
+		expect(grid.toJSON()).toMatchSnapshot();
+	});
+});
+
 describe("Render Jumbotron component correctly", () => {
 	test("Matches the snapshot", () => {
 		const jumbotron = create(<Jumbotron />);
 		expect(jumbotron.toJSON()).toMatchSnapshot();
+	});
+});
+
+describe("Render List component correctly", () => {
+	test("Matches the snapshot", () => {
+		const list = create(<List />);
+		expect(list.toJSON()).toMatchSnapshot();
+	});
+});
+
+describe("Render Nav component correctly", () => {
+	test("Matches the snapshot", () => {
+		const nav = create(<Nav />);
+		expect(nav.toJSON()).toMatchSnapshot();
 	});
 });
